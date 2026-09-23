d12eca26803a6616f53c99b346007095cf28ec77
nc-geo-64 <cola8842@vandals.uidaho.edu>
2026-09-23 10:11:17 -0600

    Refactor plotting code (#1)
    * Refactor plotting code
    
    * Ignore outut images

--- 1D_diffusion.py
+++ 1D_diffusion.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_profile(xvals, yvals, color="r", title=None, outfile=None):
+    plt.figure()
+    plt.plot(xvals, yvals, color)
+    plt.xlabel("distance")
+    plt.ylabel("elevation")
+    plt.title(title)
+    plt.savefig(outfile)
+
 D = 100
 Lx = 300
 
@@ -14,11 +22,7 @@ z_lo = 0.0
 z[x <= Lx/2] = z_hi
 z[x > Lx/2] = z_lo
 
-plt.figure()
-plt.plot(x, z, "r")
-plt.xlabel("x")
-plt.ylabel("z")
-plt.title("Initial hillslope profile")
+plot_profile(x , z, color="r", title="Initial hillslope profile", outfile="initial_profile.png")
 
 nt = 5000
 dt = 0.5 * dx**2 / D
@@ -26,9 +30,5 @@ dt = 0.5 * dx**2 / D
 for _ in range(0, nt):
 	z[1:-1] += D * dt / dx ** 2 * (z[:-2] - 2*z[1:-1] + z[2:])
 
-plt.figure()
-plt.plot(x, z, "b")
-plt.xlabel("x")
-plt.ylabel("z")
-plt.title("Final hillslope profile")
+plot_profile(x , z, color="b", title="Final hillslope profile", outfile="final_profile.png")
 
